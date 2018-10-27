0b5d6401df3b0e946e3490fc6bfc86a2e9f2dfe9
Atul Varma <varmaa@gmail.com>
2018-10-27 18:45:02 -0400

    Factor out webgl.ts.

---
+++ src/webgl.ts
@@ -0,0 +1,79 @@
+function createShader(gl: WebGLRenderingContext, type: number, source: string): WebGLShader {
+  const shader = gl.createShader(type);
+  if (!shader) throw new Error("gl.createShader() failed!");
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+  if (!success) {
+    const msg = "Compiling shader failed: " + gl.getShaderInfoLog(shader);
+    gl.deleteShader(shader);
+    throw new Error(msg);
+  }
+
+  return shader;
+}
+
+function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram {
+  const program = gl.createProgram();
+  if (!program) throw new Error("gl.createProgram() failed!");
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+    const msg = "Linking program failed: " + gl.getProgramInfoLog(program);
+    gl.deleteProgram(program);
+    throw new Error(msg);
+  }
+
+  return program;
+}
+
+function getUniformLocation(gl: WebGLRenderingContext, program: WebGLProgram, name: string): WebGLUniformLocation {
+  const loc = gl.getUniformLocation(program, name);
+
+  if (loc === null) {
+    throw new Error(`Unable to find uniform '${name}'!`);
+  }
+
+  return loc;
+}
+
+export function getAttribLocation(gl: WebGLRenderingContext, program: WebGLProgram, name: string): number {
+  const loc = gl.getAttribLocation(program, name);
+
+  if (loc === -1) {
+    throw new Error(`Unable to find attribute '${name}'!`);
+  }
+
+  return loc;
+}
+
+export class GlProgram {
+  readonly program: WebGLProgram;
+
+  constructor(readonly gl: WebGLRenderingContext, vertexShaderSrc: string, fragmentShaderSrc: string) {
+    const vertexShader = createShader(gl, WebGLRenderingContext.VERTEX_SHADER, vertexShaderSrc);
+    const fragmentShader = createShader(gl, WebGLRenderingContext.FRAGMENT_SHADER, fragmentShaderSrc);
+    this.program = createProgram(gl, vertexShader, fragmentShader);
+  }
+
+  activate() {
+    this.gl.useProgram(this.program);
+  }
+}
+
+export class GlUniformVector {
+  location: WebGLUniformLocation;
+
+  constructor(readonly program: GlProgram, name: string) {
+    this.location = getUniformLocation(program.gl, program.program, name);
+  }
+
+  set(value: Float32List) {
+    this.program.gl.uniform4fv(this.location, value);
+  }
+}
