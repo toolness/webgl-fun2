faf63d5a267f5202cf744ae84568d2231829329e
Atul Varma <varmaa@gmail.com>
2018-10-27 21:16:41 -0400

    Add comment.

--- src/simple-vertex-shader.glsl
+++ src/simple-vertex-shader.glsl
@@ -5,6 +5,8 @@ uniform float u_rotate;
 
 void main() {
   gl_Position = vec4(
+    // Deriving the rotation requires angle sum identities:
+    // https://en.wikipedia.org/wiki/List_of_trigonometric_identities#Angle_sum_and_difference_identities
     a_position.x * cos(u_rotate) - a_position.y * sin(u_rotate) + u_translate.x,
     a_position.x * sin(u_rotate) + a_position.y * cos(u_rotate) + u_translate.y,
     0.0,
