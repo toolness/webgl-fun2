e81082e6792f5a599adfad49cd41de7e8f92e903
Atul Varma <varmaa@gmail.com>
2018-11-03 18:19:33 -0400

    Screen clicks cast visible rays.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Points3D } from "./points-3d";
 import { GlProgram, getAttribLocation, GlUniformMatrix3D } from "./webgl";
 import { Points3DRenderer } from "./points-3d-renderer";
-import { Matrix3D } from "./matrix-3d";
+import { Matrix3D, Vector3D } from "./matrix-3d";
 
 const simpleVertexShaderSrc = require("./simple-vertex-shader.glsl") as string;
 const zBufferFragmentShaderSrc = require("./z-buffer-fragment-shader.glsl") as string;
@@ -31,6 +31,20 @@ function makeGround(y = -1, pointsPerAxis = 20): Points3D {
     points.push(xPart + 2, y, zPart);
     zPart += 2 * partInc;
   }
+  return Points3D.fromArray(points);
+}
+
+function makeRay(start: Vector3D, ray: Vector3D, segments = 10, length = 5): Points3D {
+  let point = start;
+  const increment = ray.normalize().times(length / segments);
+  const points: number[] = [];
+
+  for (let i = 0; i < segments; i++) {
+    points.push(point.x, point.y, point.z);
+    point = point.plus(increment);
+    points.push(point.x, point.y, point.z);
+  }
+
   return Points3D.fromArray(points);
 }
 
@@ -76,6 +90,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const program = new SimpleGlProgram(gl);
   const spaceshipRenderer = new Points3DRenderer(program, makeSpaceship());
   const groundRenderer = new Points3DRenderer(program, makeGround());
+  let rayRenderer: Points3DRenderer|null = null;
   const baseProjectionTransform = Matrix3D.perspectiveProjection({
     top: 1,
     bottom: -1,
@@ -86,21 +101,41 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   const spaceships: Spaceship[] = [];
   const NUM_SPACESHIPS = 30;
+  let screenClick: [number, number]|null = null;
+  let latestScreenClick: [number, number]|null = null;
 
   for (let i = 0; i < NUM_SPACESHIPS; i++) {
     spaceships.push(new Spaceship(-1 + ((i / NUM_SPACESHIPS) * 2)));
   }
+
+  canvas.onclick = (e) => {
+    const x = -1 + (e.offsetX / canvas.width) * 2;
+    const y = (-1 + (e.offsetY / canvas.height) * 2) * -1;
+
+    screenClick = [x, y];
+  };
 
   console.log("Initialization successful!");
 
   let cameraRotation = 0;
 
   const render = () => {
-    const viewTransform = new Matrix3D()
+    const cameraTransform = new Matrix3D()
       .rotateY(cameraRotation)
-      .translate(0, 0, 2.25)
-      .inverse();
+      .translate(0, 0, 2.25);
+    const viewTransform = cameraTransform.inverse();
     const projectionTransform = baseProjectionTransform.multiply(viewTransform);
+
+    if (screenClick && latestScreenClick !== screenClick) {
+      const inverseProjectionTransform = projectionTransform.inverse();
+      const screenPoint = new Vector3D(screenClick[0], screenClick[1], -1);
+      const screenPointInWorld = inverseProjectionTransform.transformVector(screenPoint).perspectiveDivide();
+      const cameraInWorld = cameraTransform.transformVector(new Vector3D(0, 0, 0));
+      const ray = screenPointInWorld.minus(cameraInWorld);
+
+      rayRenderer = new Points3DRenderer(program, makeRay(cameraInWorld, ray));
+      latestScreenClick = screenClick;
+    }
 
     cameraRotation += 0.001;
 
@@ -109,6 +144,11 @@ window.addEventListener('DOMContentLoaded', () => {
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     program.activate();
+    if (rayRenderer) {
+      rayRenderer.setupForDrawing();
+      program.transform.set(projectionTransform);
+      rayRenderer.draw(gl.LINES);
+    }
     groundRenderer.setupForDrawing();
     program.transform.set(projectionTransform);
     groundRenderer.draw(gl.LINES);
--- src/matrix-3d.ts
+++ src/matrix-3d.ts
@@ -39,6 +39,33 @@ const IDENTITY: Matrix3DTuple = [
 export class Vector3D {
   constructor(readonly x: number, readonly y: number, readonly z: number, readonly w: number = 1.0) {
   }
+
+  perspectiveDivide(): Vector3D {
+    const { x, y, z, w } = this;
+    return new Vector3D(x / w, y / w, z / w);
+  }
+
+  minus(v: Vector3D): Vector3D {
+    const { x, y, z, w } = this;
+    return new Vector3D(x - v.x, y - v.y, z - v.z, w);
+  }
+
+  plus(v: Vector3D): Vector3D {
+    const { x, y, z, w } = this;
+    return new Vector3D(x + v.x, y + v.y, z + v.z, w);
+  }
+
+  times(n: number): Vector3D {
+    const { x, y, z, w } = this;
+    return new Vector3D(x * n, y *n, z * n, w);
+  }
+
+  normalize(): Vector3D {
+    const { x, y, z, w } = this;
+
+    const len = Math.sqrt(x * x + y * y + z * z);
+    return new Vector3D(x / len, y / len, z / len, w);
+  }
 }
 
 /**
