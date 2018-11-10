5660b73f1b24593b0c476d89922a4bd4b81262db
Atul Varma <varmaa@gmail.com>
2018-11-10 17:27:44 -0500

    Remove Matrix3D.fromFloat32Array().

--- src/matrix-3d.ts
+++ src/matrix-3d.ts
@@ -101,19 +101,6 @@ export class Matrix3D {
     ]);
   }
 
-  /**
-   * Convert the given float array, in column-major order, to a
-   * matrix.
-   */
-  static fromFloat32Array(m: Float32Array): Matrix3D {
-    return new Matrix3D([
-      [m[0], m[4], m[8], m[12]],
-      [m[1], m[5], m[9], m[13]],
-      [m[2], m[6], m[10], m[14]],
-      [m[3], m[7], m[11], m[15]]
-    ]);
-  }
-
   valueAt(row: Row, column: Column): number {
     return this.values[row - 1][column - 1];
   }
