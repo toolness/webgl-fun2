cd1acba652121bc9983cc9f8be1e49a5a7867643
Atul Varma <varmaa@gmail.com>
2018-11-10 17:34:42 -0500

    Restore whitespace.

--- src/shapes.ts
+++ src/shapes.ts
@@ -7,6 +7,7 @@ export function makeSpaceship(): Points3D {
     0, 0.75, 0,
     0, 0.15, 0
   ]);
+
   return leftHalf.concat(leftHalf.mirrorHorizontally());
 }
 
@@ -15,17 +16,20 @@ export function makeGround(y = -1, pointsPerAxis = 20): Points3D {
   let xPart = -1;
   let zPart = -1;
   let partInc = 1 / pointsPerAxis;
+
   for (let i = 0; i <= pointsPerAxis; i++) {
     points.push(xPart, y, zPart);
     points.push(xPart, y, zPart + 2);
     xPart += 2 * partInc;
   }
+
   xPart = -1;
   for (let i = 0; i <= pointsPerAxis; i++) {
     points.push(xPart, y, zPart);
     points.push(xPart + 2, y, zPart);
     zPart += 2 * partInc;
   }
+
   return Points3D.fromArray(points);
 }
 
@@ -33,10 +37,12 @@ export function makeRay(start: Vector3D, ray: Vector3D, segments = 10, length = 
   let point = start;
   const increment = ray.normalize().times(length / segments);
   const points: number[] = [];
+
   for (let i = 0; i < segments; i++) {
     points.push(point.x, point.y, point.z);
     point = point.plus(increment);
     points.push(point.x, point.y, point.z);
   }
+
   return Points3D.fromArray(points);
 }
