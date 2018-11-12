7242e46feb654ba9abc8d08f91e86d4340d648c0
Atul Varma <varmaa@gmail.com>
2018-11-12 08:10:46 -0500

    Make the vertex shader pass the color to the fragment shader.

--- src/simple-fragment-shader.glsl
+++ src/simple-fragment-shader.glsl
@@ -1,7 +1,8 @@
 precision mediump float;
 
 uniform bool u_show_z_buffer;
-uniform vec4 u_color;
+
+varying vec4 v_color;
 
 void main() {
   if (u_show_z_buffer) {
@@ -10,6 +11,6 @@ void main() {
     //   https://learnopengl.com/Advanced-OpenGL/Depth-testing
     gl_FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
   } else {
-    gl_FragColor = u_color;
+    gl_FragColor = v_color;
   }
 }
--- src/simple-vertex-shader.glsl
+++ src/simple-vertex-shader.glsl
@@ -1,7 +1,11 @@
 attribute vec4 a_position;
 
 uniform mat4 u_transform;
+uniform vec4 u_color;
+
+varying vec4 v_color;
 
 void main() {
   gl_Position = u_transform * a_position;
+  v_color = u_color;
 }
